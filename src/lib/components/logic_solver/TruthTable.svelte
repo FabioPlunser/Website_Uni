ecc5e1d8ecfae1e12329e70b97f14452a02b3d7a
Fabio Plunser <f.plunser@outlook.com>
2022-11-17 17:00:37 +0100

    added clear table button

--- src/lib/components/logic_solver/TruthTable.svelte
+++ src/lib/components/logic_solver/TruthTable.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+
+
 	// Input Variables
 	export let input_names: String[];
 	export let output_names: String[];
@@ -21,45 +23,48 @@
 	}
 
 	$: values = v;
+
+	
 </script>
 
 {#if input_names != null && output_names != null}
-<div class="flex overflow-x-auto">
-	<table class="table table-zebra table-fixed border-seperate">
-		<thead>
-			<tr>
-				{#each input_names as name}
-					<th>{name}</th>
-				{/each}
-				{#each output_names as name}
-					<th>{name}</th>
-				{/each}
-			</tr>
-		</thead>
-		<tbody>
-			{#each { length: 2 ** num_inputs } as _, row}
-				<tr class="hover rounded-ld">
-					{#each { length: num_inputs } as _, i}
-						<td>
-							{boolToString((row & (2 ** (num_inputs - 1 - i))) != 0)}
-						</td>
+	<div class="flex overflow-x-auto">
+		<table class="table table-zebra table-fixed border-seperate">
+			<thead>
+				<tr>
+					{#each input_names as name}
+						<th>{name}</th>
 					{/each}
-					{#each { length: num_outputs } as _, i}
-						{#if editable}
-							<td class="cursor-pointer hover:text-gray-500"
-								on:click={(e) => (values[i][row] = !values[i][row])}
-							>
-								{boolToString(values[i][row])}
-							</td>
-						{:else}
-							<td>
-								{boolToString(values[i][row])}
-							</td>
-						{/if}
+					{#each output_names as name}
+						<th>{name}</th>
 					{/each}
 				</tr>
-			{/each}
-		</tbody>
-	</table>
-</div>
+			</thead>
+			<tbody>
+				{#each { length: 2 ** num_inputs } as _, row}
+					<tr class="hover rounded-ld">
+						{#each { length: num_inputs } as _, i}
+							<td>
+								{boolToString((row & (2 ** (num_inputs - 1 - i))) != 0)}
+							</td>
+						{/each}
+						{#each { length: num_outputs } as _, i}
+							{#if editable}
+								<td class="cursor-pointer hover:text-gray-500"
+									on:click={(e) => (values[i][row] = !values[i][row])}
+								>
+									{boolToString(values[i][row])}
+								</td>
+							{:else}
+								<td>
+									{boolToString(values[i][row])}
+								</td>
+							{/if}
+						{/each}
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
 {/if}
+
--- src/routes/logic_solver/+page.svelte
+++ src/routes/logic_solver/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { browser } from "$app/environment";
+
 	import TruthTable from "$components/logic_solver/TruthTable.svelte";
 	import InputSelector from "$components/logic_solver/InputSelector.svelte";
 	import Algebra from "$components/logic_solver/algebra.svelte";
@@ -6,13 +8,23 @@
 	let input_names: String[];
 	let output_names: String[];
 
+	function clearTable(){
+		for(let i = 0; i < output_names.length; i++){
+			for(let j = 0; j < 2 ** input_names.length; j++){
+				values[i][j] = false;
+			}
+		}
+	}
 </script>
 
 
 
 <main class="m-5 w-full h-full">
 	<div class="flex">
-		<TruthTable {input_names} {output_names} editable={true} bind:values />
+		<div>
+		<button class="btn btn-primary" on:click={()=>clearTable()} >Clear Table</button>
+			<TruthTable {input_names} {output_names} editable={true} bind:values />
+		</div>
 		<InputSelector bind:input_names bind:output_names />
 		<Algebra {input_names} {output_names} {values}/>
 	</div>
