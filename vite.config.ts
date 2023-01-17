7095d1308e8b2cbb77d4a3bb1511468a5900cd11
Fabio Plunser <f.plunser@outlook.com>
2023-01-17 12:49:31 +0100

    removed all unecessary packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
-	resolve: {
-		dedupe: ['@fullcalendar/common'],
-	},
-	optimizeDeps: {
-		include: ['@fullcalendar/common', "highlight.js", "highlight.js/lib/core"],
-	}
 };
 
 export default config;
