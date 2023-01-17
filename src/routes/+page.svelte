f1ec312e9f0e909aa7a5ca818efa235c90c110ed
Fabio Plunser <f.plunser@outlook.com>
2023-01-18 00:12:10 +0100

    added cards, and steps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,65 @@
 <script lang="ts">
-
+    import transistor from "$assets/transistor.svg";
+    import memori from "$assets/memori.svg";
 </script>
 
 <section>
     <div class="flex items-center justify-center">
-        <div class="text-2xl text-center">
+        <div class="font-bold text-2xl text-center">
             <h1>UIBK Help</h1>
             <span>
                 A website to help students 
                 pick their courses and provide 
-                nice to haves for especially computer science students
+                nice to haves, especially for computer science students
             </span>
+        </div>
+        
+      
+    </div>
+
+    <div class="mt-24 flex flex-col sm:flex-row gap-10 justify-center">
+        <div class="card w-auto bg-green-400 dark:bg-green-700 shadow-2xl rotate-3 max-w-md">
+            <div class="card w-auto bg-base-200 shadow text-black dark:text-white -rotate-3">
+                <div class="card-body items-center text-center break-all">
+                    <h2 class="card-title">Course Selector <i class="bi bi-calendar2-week"></i> </h2>
+                    
+                    <p>
+                        A site to add courses to a simple Calendar to see if courses clash with each other 
+                    </p>
+                    <div class="card-actions justify-end">
+                        <a href="/course-selector" class="btn btn-primary">Go</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card w-auto bg-green-400 dark:bg-green-700 shadow-2xl rotate-3 max-w-md">
+            <div class="card w-auto bg-base-200 shadow text-black dark:text-white -rotate-3">
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">Logic Solver <img src={transistor} alt="logic solver logic" class="w-8 h-auto" /></h2>
+                    <p>
+                        A site where you can config a truth table and CCNF, DDNF and KV diagram are 
+                        generated automatically
+                    </p>
+                    <div class="card-actions justify-end">
+                        <a href="/logic-solver" class="btn btn-primary">Go</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card w-auto bg-green-400 dark:bg-green-700 shadow-2xl rotate-3 max-w-md">
+            <div class="card w-auto bg-base-200 shadow text-black dark:text-white -rotate-3">
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">Memori <img src={memori} alt="logic solver logic" class="w-8 h-auto" /></h2>
+                    <p>
+                        A site where you can create decks of flashcards and test yourself
+                    </p>
+                    <div class="card-actions justify-end">
+                        <a href="/memori" class="btn btn-primary">Go</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
---
+++ src/routes/course-selector/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+
+    enum Step {
+        FieldOfStudy,
+        Curriculum,
+        CourseType,
+        Group
+    }
+    $: console.log(selected);
+    let selected = Step.FieldOfStudy;
+</script>
+
+<section>
+    <div class="flex justify-center mx-auto items-center">
+        {#if selected < 4}
+            <ul class="steps"> 
+                <li class="step {selected===Step.FieldOfStudy && "step-primary"}">Choose field of Study</li>
+                <li class="step {selected===Step.Curriculum && "step-primary"}">Choose curriculum</li>
+                <li class="step {selected===Step.CourseType && "step-primary"}">Choose course type</li>
+                <li class="step {selected===Step.Group && "step-primary"}">Choose group</li>
+            </ul>  
+        {/if}
+    </div>
+    <input type="text" placeholder="Type Here" class="mt-20 flex mx-auto justify-center input w-full bg-base-300 max-w-xs" />
+    <button on:click={()=>selected++} class="btn btn-primary mt-5 flex mx-auto justify-center w-full max-w-xs">Next</button>
+</section>
