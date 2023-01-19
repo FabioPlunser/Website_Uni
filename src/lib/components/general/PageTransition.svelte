e7391263adfb439d2cdfae6e82b328544ba96ee8
Fabio Plunser <f.plunser@outlook.com>
2023-01-19 16:52:49 +0100

    added rust fix

---
+++ src/lib/components/general/PageTransition.svelte
@@ -0,0 +1,12 @@
+<!-- PageTransition.svelte -->
+<script lang="ts">
+    import { fly } from "svelte/transition";
+    export let url:any = null;
+    const pageTransitionDuration = 500;
+</script>
+  
+{#key url}
+  <div in:fly={{y:-50, duration: 500}} >
+    <slot />
+  </div>
+{/key}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
     import '../app.css';
     import Nav from "$lib/components/general/Nav.svelte";
+    import PageTransition from '$lib/components/general/PageTransition.svelte';
+    import { page } from "$app/stores"
 </script>
 
 <Nav/>
 <div class="divider m-0"/>
 <main class="m-10">
-    <slot />
+    <PageTransition url={$page.url}>
+        <slot />
+    </PageTransition>
 </main>
 
